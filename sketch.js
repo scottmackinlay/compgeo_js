990453fe638238ffcde44ebe0d8356f7943d2a36
Scott <scottmackinlay@machinalabs.ai>
2021-06-12 20:31:14 -0700

    first commit part two

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+
+points = []
+
+
+
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+  for (let i=0; i<10; i++){
+    points.push(createVector(random(width), random(height)))
+  }
 }
 
 function draw() {
-  background(220);
+  background(100);
+  stroke(255)
+  strokeWeight(8)
+  for (let p of points){
+    point(p.x, p.y)
+  }
+
 }
